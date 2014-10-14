dd0039b3e6e577cd46a60d4c2637620d4ff84ec6
James Garcia <james.m.garcia7@gmail.com>
2014-10-14 11:45:01 -0500

    submitting edit

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -17,15 +17,30 @@ var_dump($_POST)
 	<p>
         <label for="password">Password</label>
         <input id="password" name="password" type="password" placeholder="Password">
-    </p>
-    <p>
-		<label for="name">Name</label>
-        <input id="name" name="name" type="text" placeholder="Enter your Name">
      </p>
      <p>
      	<button type="submit">Let's a go!</button>
     </p>
 </form>
 
+<form method="POST" action="my_first_form.php">
+	<p>
+		<label for="to">To:</label>
+		<input id="to" name="to" type="text" placeholder="Enter Recipient">
+	</p>
+	<p>
+		<label for="from">From:</label>
+		<input id="from" name="from" type="text" placeholder="Enter Sender">
+	</p>
+	<p>
+		<label for="subject">Subject:</label>
+		<input id="subject" name="subject" type="text" placeholder="Enter Subject">
+	</p>
+	<p>
+		<textarea id="body" name="body" type="text" placeholder="Enter Email Body"></textarea>
+	</p>
+	<p>
+	<button type="submit">Send!</button>
+	</p>
 </body>
 </html>

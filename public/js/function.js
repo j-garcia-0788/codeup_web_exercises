1e9b565098ea015f8a0335a8e481b945ad2cea3f
James Garcia <james.m.garcia7@gmail.com>
2014-10-21 13:04:24 -0500

    function js

---
+++ public/js/function.js
@@ -0,0 +1,16 @@
+var myNameIs = 'James';
+
+function sayHello(name) {
+    console.log(name + " is still figgering out functions on his own! :)");
+}
+
+sayHello(myNameIs)
+
+var random = Math.floor((Math.random()*100)+1);
+
+function isOdd(random) {
+	var random = Math.floor((Math.random()*100)+1);
+	var message = (random % 2 != 0) ? random + ' Is odd' : random + ' is even';
+	console.log(message)
+}
+isOdd(random)

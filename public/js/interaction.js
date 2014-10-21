54e8886aaf374a226946edc7f704613beb70c3d7
James Garcia <james.m.garcia7@gmail.com>
2014-10-21 14:18:20 -0500

    user interaction

---
+++ public/js/interaction.js
@@ -0,0 +1,21 @@
+// todo: Ask the user for their name.
+//       Keep asking if an empty input is provided.
+
+// todo: Show an alert message that welcomes the user based on their input.
+
+// todo: Ask the user if they like pizza.
+//       Based on their answer tell show a creative alert message.
+
+do {
+	var response = prompt('What is your name?');
+} while (response == '' || response == null);
+	
+	console.log(response);
+
+	alert('Hello, ' + response);
+
+
+	var pizza = confirm('Do you enjoy pizza?');
+
+	alert(confirm == true ? 'Pizza is always interesting' : 'Get Out');
+

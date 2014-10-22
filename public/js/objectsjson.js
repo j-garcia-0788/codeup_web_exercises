c7d5e3b63bfd2609f818dad44985c5fceb2902f8
James Garcia <james.m.garcia7@gmail.com>
2014-10-22 15:56:18 -0500

    Objects in Json

---
+++ public/js/objectsjson.js
@@ -0,0 +1,49 @@
+// todo:
+// Create an array of objects that represent books.
+// Each book should have a title and an author.
+// The book author should be an object with a firstName and lastName.
+// Be creative and add at least 5 books to the array
+// var books = [todo];
+
+var books = [
+	{"Title": 'Jurassic Park', 
+	"Author": {
+				"First": 'Michael',
+				"Last": 'Crichton'}
+				},
+	{"Title": 'The Lost World',
+	"Author": {
+				"First": 'Michael',
+				"Last": 'Crichton'}
+			},
+	{"Title": 'The Fellowship of the Ring',
+	"Author": {
+				"First": 'JR',
+				"Last": 'Tolkien'}
+			},
+	{"Title": 'The Towers',
+	"Author": {
+				"First": 'JR',
+				"Last": 'Tolkien'}
+			},
+	{"Title": 'Return of the King',
+	"Author": {
+				"First": 'JR',
+				"Last": 'Tolkien'}
+			}]
+
+// log out the books array
+console.log(books);
+
+// todo:
+// Loop through the array of books using .forEach and print out the specified information about each one.
+// start loop here
+books.forEach(function (books, index) {
+
+    console.log("Book #" + (index + 1));
+    console.log("Title: " + (books.Title));
+    console.log("Author: " + (books.Author['First'] + ' ' + books.Author['Last']));
+    console.log("---")
+
+});
+// end loop here
